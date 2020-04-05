53c6b229466b17966d6342b06aa9c4ec0631d62b
trina <31892809+kateshostak@users.noreply.github.com>
2020-04-05 23:21:24 +0300

    5th

--- 5_best_time_to_buy_and_sell.py
+++ 5_best_time_to_buy_and_sell.py
@@ -5,18 +5,10 @@ import pdb
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_sum = 0
-
-
         for i in range(len(prices) - 1):
-            tmp_sum = 0
-            for j in range(i + 1, len(prices)):
-                diff = prices[j] - prices[i]
-                print(f'i::{i}, j::{j}, diff::{diff}')
-                if diff > 0:
-                    tmp_sum += diff
-                    i = j + 1
-                    continue
-            max_sum = max(tmp_sum, max_sum)
+            diff = prices[i+1] - prices[i]
+            if diff > 0 :
+                max_sum += diff
         return max_sum
 
 def main():
