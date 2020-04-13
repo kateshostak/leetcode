9843a46c160ee752dfdd155fd14e02038468a3d5
trina <31892809+kateshostak@users.noreply.github.com>
2020-04-14 00:14:12 +0300

    exceeded time limit

--- 13_contiguous_array.py
+++ 13_contiguous_array.py
@@ -3,36 +3,31 @@ import pdb
 
 
 class Solution:
-    def findMaxLength(self, nums: List[int]) -> int:
-        sentiel = nums[0]
-        arr = []
-        i = 1
-        for elem in nums[1:]:
-            if elem != sentiel:
-                arr.append(i)
-                sentiel = elem
-                i = 1
+    def findMaxLength(self, nums: List[int]) -> int: # noqa
+        max_ = 0
+        for i in range(len(nums) - 1):
+            if nums[i] == 0:
+                sum_ = -1
             else:
-                i += 1
-        arr.append(i)
-        max_arr = 0
-        for j in range(len(arr) - 1):
-            tmp_max = min(arr[j], arr[j + 1])
-            max_arr = max(tmp_max, max_arr)
-
-        return max_arr*2
-
+                sum_ = 1
+            for j in range(i + 1, len(nums)):
+                if nums[j] == 0:
+                    sum_ -= 1
+                else:
+                    sum_ += 1
+                if sum_ == 0:
+                    tmp_max = j - i + 1
+                    max_ = max(tmp_max, max_)
+        return max_
 
 def main():
     arr = [0, 1]
-    arr = [0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1]
-    arr = [0, 1, 1]
-    arr = [0, 1, 0, 1]
+    # arr = [0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1]
+    # arr = [0, 1, 1]
+    # arr = [0, 1, 0, 1]
     s = Solution()
     print(s.findMaxLength(arr))
 
 
 if __name__ == '__main__':
     main()
-
-
