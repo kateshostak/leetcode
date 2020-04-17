58c110cb655b0e517214690d167a108817def193
trina <31892809+kateshostak@users.noreply.github.com>
2020-04-17 22:22:37 +0300

    16th

--- 16_valid_parenthesis_string.py
+++ 16_valid_parenthesis_string.py
@@ -1,43 +1,18 @@
 import pdb
 
 
-class Solution:
-    def checkValidString(self, s: str) -> bool:
-        stars = 0
-        sum_ = 0
-        stars = self.count_stars(s)
-        stars_pref = 0
-        stars_suff = stars
-        symbol_i =0
-        star_i = 0
-        for i, symbol in enumerate(s):
-            if symbol == '(':
-                symbol_i = i
-                sum_ += 1
-            elif symbol == ')':
-                symbol_i = i
-                sum_ -= 1
-            else:
-                star_i = i
-                stars_pref += 1
-                stars_suff -= 1
+class Solution():
+    def checkValidString(self, s):
+        low = high = 0
+        for c in s:
+            low += 1 if c == '(' else -1
+            high += 1 if c != ')' else -1
+            if high < 0:
+                break
+            low = max(low, 0)
 
-            if sum_ < 0:
-                if stars_pref > 0:
-                    sum_ += 1
-                    stars_pref -= 1
-                else:
-                    return False
+        return low == 0
 
-        if sum_ == 0:
-            return True
-        else:
-            if sum_ == star_i - symbol_i:
-                return True
-        return False
-
-    def count_stars(self, s):
-        return sum([1 for symbol in s if symbol == '*'])
 def main():
     # Input: "()"
     # Output: True
@@ -47,7 +22,7 @@ def main():
 
     # Input: "(*))"
     # Output: True
-    s = ''
+    s = '(*()'
     print(Solution().checkValidString(s))
 
 
