1479666bd96103fddd46ced8d70adbc68734e69a
trina <31892809+kateshostak@users.noreply.github.com>
2020-04-21 17:15:19 +0300

    20th

--- 20_construct_binary_tree_from_preoder_traversal.py
+++ 20_construct_binary_tree_from_preoder_traversal.py
@@ -1,29 +1,46 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from typing import List
+import pdb
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return self.val
+
 
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
         root = TreeNode(preorder[0])
-        def add_node(root, val):
-            tmp = TreeNode(val)
-            curr = root
-            while True:
-                if val < curr.val:
-                    if curr.left is None:
-                        curr.left = tmp
-                        break
-                    else:
-                        curr = curr.left
-                else:
-                    if curr.right is None:
-                        curr.right = tmp
-                        break
-                    else:
-                        curr = curr.right
-        for val in preorder[1:]:
-            add_node(root, val)
+        print(f'len = {len(preorder)}')
+        pdb.set_trace()
+
+        def traverse(grandparent, parent, i):
+            if grandparent:
+                print(f'i:{i}')
+                if i > len(preorder) - 1 or preorder[i] > grandparent.val:
+                    print('out')
+                    return
+
+
+            if preorder[i] > parent.val:
+                parent.right = TreeNode(preorder[i])
+                traverse(parent, parent.right, i + 1)
+            else:
+                parent.left = TreeNode(preorder[i])
+                traverse(parent, parent.left, i + 1)
+            traverse(grandparent, parent, i + 1)
+        traverse(None, root, 1)
         return root
+
+
+def main():
+    arr = [8, 5, 1, 7, 10, 12]
+    print(Solution().bstFromPreorder(arr))
+
+
+if __name__ == '__main__':
+    main()
