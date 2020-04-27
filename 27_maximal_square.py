803fb22d291e8f1dfbe1247fcf2bb876e0b5929b
trina <31892809+kateshostak@users.noreply.github.com>
2020-04-27 23:25:04 +0300

    wip

--- 27_maximal_square.py
+++ 27_maximal_square.py
@@ -9,27 +9,28 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if int(matrix[i][j]):
+
                     size = 1
-                    min_ = 1
-                    i2 = i + 1
-                    j2 = j - 1
                     step = 1
                     is_square = True
-                    while is_square and j2 >= 0 and i2 < len(matrix):
-                        size += 1
+                    while is_square and j + step < len(matrix[i]) and i + step < len(matrix):
+                        if int(matrix[i + step][j + step]):
 
-                        if int(matrix[i2][j2]):
-                            for i3 in range(i2 - i):
-                                if not int(matrix[i + i3][j2]):
+                            size += 1
+                            for delta in range(step + 1):
+                                if not int(matrix[i + delta][j + step]):
                                     is_square = False
                                     size -= 1
+                                    break
 
-                            for j3 in range(j2 - j):
-                                if not int(matrix[i2][j + j3]):
+                            if not is_square:
+                                break
+
+                            for delta in range(step + 1):
+                                if not int(matrix[i + step][j + delta]):
                                     is_square = False
                                     size -= 1
-                        i2 += 1
-                        j2 -= 1
+                        step += 1
                     max_ = max(size, max_)
 
         return max(max_*max_, min_)
@@ -51,10 +52,7 @@ def main():
             ]
     # out = 4
 
-    matrix = [
-            [1, 1],
-            [1, 1]
-            ]
+    matrix = [[1, 1], [1, 1]]
     print(Solution().maximalSquare(matrix))
 
 
