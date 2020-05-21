2716e010155c33391bd4b108c808b182a89870fc
trina <31892809+kateshostak@users.noreply.github.com>
2020-05-22 00:22:12 +0300

    1277

--- count_square_submatrices_with_all_ones.py
+++ count_square_submatrices_with_all_ones.py
@@ -24,11 +24,6 @@ class Solution:
                 k += elem
                 c_m[i][j] = k
 
-        def mark_checked(start_i, start_j, delta):
-            for i in range(delta):
-                matrix[start_i + delta][start_j + i] = 2
-                matrix[start_i + i][start_j + delta] = 2
-
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if matrix[i][j] == 1:
@@ -37,11 +32,6 @@ class Solution:
                     i_1 = i
                     j_1 = j
                     while k == n and i_1 < len(matrix) and j_1 < len(matrix[0]):
-                        matrix[i_1][j_1] = 2
-                        mark_checked(i, j, k)
-                        print()
-                        for row in matrix:
-                            print(row)
                         k += 1
                         n = min(r_m[i_1][j_1], c_m[i_1][j_1])
                         n = min(k, n)
@@ -51,21 +41,7 @@ class Solution:
                         res.append(k)
                     else:
                         res.append(k - 1)
-
-        def count_squares(n):
-            i = 0
-            res = 0
-            while i < n:
-                res += (n - i)*(n - i)
-                i += 1
-            return res
-
-        res2 = []
-        for elem in res:
-            res2.append(count_squares(elem))
-
-        print(res)
-        print(res2)
+        return sum(res)
 
 
 def main():
@@ -75,8 +51,15 @@ def main():
         [0, 1, 1, 1]
     ]
     res = Solution().countSquares(matrix)
-    print(f'expected::???, got::{res}')
+    print(f'expected::15, got::{res}')
 
+    matrix = [
+        [1, 0, 1],
+        [1, 1, 0],
+        [1, 1, 0]
+    ]
+    res = Solution().countSquares(matrix)
+    print(f'expected::7, got::{res}')
 
 if __name__ == '__main__':
     main()
