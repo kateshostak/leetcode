3b248c66df2484f0e0c767684e73ac2f32bc8cce
trina <31892809+kateshostak@users.noreply.github.com>
2020-05-29 02:09:06 +0300

    better solution

--- counting_bits.py
+++ counting_bits.py
@@ -3,31 +3,17 @@ from typing import List
 
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        if num == 0:
-            return [0]
-        elif num == 1:
-            return [0, 1]
-        elif num == 2:
-            return [0, 1, 1]
-        elif num == 3:
-            return [0, 1, 1, 2]
+        res = []
+        pow_ = 0
+        for i in range(num + 1):
+            if not(i & (i - 1)):
+                pow_ = i
 
-        res = [0, 1, 1, 2]
-        i = 1
-        j = 2
-
-        while len(res) <= num + 1:
-            start = 2**i
-            end = 2**j - 1
-            while start < end:
-                for k in range(start, end + 1):
-                    res.append(res[k])
-                start = (start + end)//2 + 1
-            res.append(res[end])
-            res.append(res[-1] + 1)
-            i += 1
-            j += 1
-        return res[:num + 1]
+            if not res:
+                res.append(0)
+            else:
+                res.append(1 + res[i - pow_])
+        return res
 
 
 def main():
@@ -37,7 +23,6 @@ def main():
 
     n = 5
     res = Solution().countBits(n)
-    print(f'expected::[0, 1, 1, 2, 1, 2], got::{res}')
 
 
 if __name__ == '__main__':
