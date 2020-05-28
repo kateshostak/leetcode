3b5593178e683f4cb4e178f155b84587e23472db
trina <31892809+kateshostak@users.noreply.github.com>
2020-05-28 19:39:24 +0300

    wip

--- counting_bits.py
+++ counting_bits.py
@@ -1,9 +1,34 @@
 from typing import List
+import math
 
 
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        return [sum(map(int, format(elem, 'b'))) for elem in range(num + 1)]
+        if num == 0:
+            return [0]
+        elif num == 1:
+            return [0, 1]
+        elif num == 2:
+            return [0, 1, 1]
+        elif num == 3:
+            return [0, 1, 1, 2]
+
+        res = [0, 1, 1, 2]
+        start = 2
+        end = 3
+        n = math.floor(math.log2(num)) + 1
+
+        while end <= n + 1:
+            while start < end:
+                for i in range(start, end):
+                    print(start)
+                    res.append(res[i])
+                    start = (start + end)//2 + 1
+            res.append(res[end])
+            res.append(res[-1] + 1)
+            start += 1
+            end += 1
+        return res
 
 
 def main():
@@ -16,6 +41,7 @@ def main():
     print(f'expected::[0, 1, 1, 2, 1, 2], got::{res}')
 
     print(Solution().countBits(32))
+
 
 if __name__ == '__main__':
     main()
