678bfa38096983c6ebba687c8cb45959f509ec12
trina <31892809+kateshostak@users.noreply.github.com>
2020-05-28 18:00:48 +0300

    1277

--- count_square_submatrices_with_all_ones.py
+++ count_square_submatrices_with_all_ones.py
@@ -3,45 +3,13 @@ from typing import List
 
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
-        r_m = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
-        c_m = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
-        res = []
-        for i in range(len(matrix)):
-            k = 0
-            for j in range(len(matrix[i])):
-                elem = matrix[i][j]
-                if elem == 0:
-                    k = 0
-                k += elem
-                r_m[i][j] = k
-
-        for j in range(len(matrix[0])):
-            k = 0
-            for i in range(len(matrix)):
-                elem = matrix[i][j]
-                if elem == 0:
-                    k = 0
-                k += elem
-                c_m[i][j] = k
-
+        res = 0
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                if matrix[i][j] == 1:
-                    k = 0
-                    n = 0
-                    i_1 = i
-                    j_1 = j
-                    while k == n and i_1 < len(matrix) and j_1 < len(matrix[0]):
-                        k += 1
-                        n = min(r_m[i_1][j_1], c_m[i_1][j_1])
-                        n = min(k, n)
-                        i_1 += 1
-                        j_1 += 1
-                    if k == n:
-                        res.append(k)
-                    else:
-                        res.append(k - 1)
-        return sum(res)
+                if matrix[i][j] == 1 and i >= 1 and j >= 1:
+                    matrix[i][j] = min(matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]) + 1
+                res += matrix[i][j]
+        return res
 
 
 def main():
@@ -60,6 +28,7 @@ def main():
     ]
     res = Solution().countSquares(matrix)
     print(f'expected::7, got::{res}')
+
 
 if __name__ == '__main__':
     main()
