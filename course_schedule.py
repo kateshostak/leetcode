9cb4185e6af5ff17cdf1cc1695aa9ced0bd60d58
trina <31892809+kateshostak@users.noreply.github.com>
2020-05-30 01:59:33 +0300

    wip

--- course_schedule.py
+++ course_schedule.py
@@ -4,25 +4,27 @@ from typing import List
 
 class Solution(object):
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
         graph = collections.defaultdict(list)
+        color = {}
         for u, v in prerequisites:
             graph[u].append(v)
-
-        color = {}
+            color[u] = 'w'
+            color[v] = 'w'
 
         def dfs(node):
-            if node in color:
+            if node in color and color[node] == 'g':
                 return True
             color[node] = 'g'
             for elem in graph[node]:
-                if elem in color and color[elem] != 'b':
-                    dfs(elem)
-                color[node] = 'b'
+                if color[elem] != 'b':
+                    if dfs(elem):
+                        return True
+            color[node] = 'b'
             return False
 
-        return all(dfs(node)
-                   for node in range(numCourses)
-                   if node not in color)
+        return not all(dfs(node) for node in graph)
 
 
 def main():
@@ -33,6 +35,21 @@ def main():
 
     numCourses = 2
     prerequisites = [[1, 0], [0, 1]]
+    res = Solution().canFinish(numCourses, prerequisites)
+    print(f'expected::False, got::{res}')
+
+    numCourses = 3
+    prerequisites = [[1, 0], [2, 1]]
+    res = Solution().canFinish(numCourses, prerequisites)
+    print(f'expected::True, got::{res}')
+
+    numCourses = 3
+    prerequisites = [[0, 1], [0, 2], [1, 2]]
+    res = Solution().canFinish(numCourses, prerequisites)
+    print(f'expected::True, got::{res}')
+
+    numCourses = 4
+    prerequisites = [[2, 0], [1, 0], [3, 1], [3, 2], [1, 3]]
     res = Solution().canFinish(numCourses, prerequisites)
     print(f'expected::False, got::{res}')
 
