602b0badfb101e8f105714d4200228f2d101287a
trina <31892809+kateshostak@users.noreply.github.com>
2020-06-03 02:37:22 +0300

    237

---
+++ delete_node_in_linked_list.py
@@ -0,0 +1,71 @@
+import pdb
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        tmp = node
+        while node.next:
+            node.val = node.next.val
+            node = node.next
+            if node.next:
+                tmp = tmp.next
+        tmp.next = None
+
+    def as_arr(self, node):
+        res = []
+        while node:
+            res.append(node.val)
+            node = node.next
+        return res
+
+
+def main():
+    arr = [4, 5, 1, 9]
+    n = 5
+    tmp = head = ListNode(4)
+    for elem in arr[1:]:
+        tmp.next = ListNode(elem)
+        tmp = tmp.next
+    node = head.next
+
+    Solution().deleteNode(node)
+    res = Solution().as_arr(head)
+    print(f'expected::[4, 1, 9], got::{res}')
+
+    arr = [4, 5, 1, 9]
+    n = 1
+    tmp = head = ListNode(4)
+    for elem in arr[1:]:
+        tmp.next = ListNode(elem)
+        tmp = tmp.next
+    node = head.next.next
+
+    Solution().deleteNode(node)
+    res = Solution().as_arr(head)
+    print(f'expected::[4, 5, 9], got::{res}')
+
+    arr = [2, 0, 1, 3]
+    n = 2
+    tmp = head = ListNode(2)
+    for elem in arr[1:]:
+        tmp.next = ListNode(elem)
+        tmp = tmp.next
+    node = head
+
+    Solution().deleteNode(node)
+    res = Solution().as_arr(head)
+    print(f'expected::[0, 1, 3], got::{res}')
+
+
+if __name__ == '__main__':
+    main()
