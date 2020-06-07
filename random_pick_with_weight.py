4b959e7aef98424fdc582b07e8701093632b7bc2
trina <31892809+kateshostak@users.noreply.github.com>
2020-06-07 03:26:31 +0300

    528

--- random_pick_with_weight.py
+++ random_pick_with_weight.py
@@ -5,13 +5,21 @@ from typing import List
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.box = []
+        self.start_to_index = {}
+        self.start_to_end = {}
+        start = 0
+        self.len_ = 0
         for i, elem in enumerate(w):
-            self.box += [i]*elem
+            self.start_to_index[start] = i
+            self.start_to_end[start] = start + elem
+            start += elem
+            self.len_ = start
 
     def pickIndex(self) -> int:
-        j = random.randint(0, len(self.box) - 1)
-        return(self.box[j])
+        j = random.randint(0, self.len_ - 1)
+        for start, end in self.start_to_end.items():
+            if j >= start and j < end:
+                return self.start_to_index[start]
 
 
 def main():
