cff84823eaad83e9b976c1e9f81090b9ce266ba0
trina <31892809+kateshostak@users.noreply.github.com>
2020-06-10 00:11:54 +0300

    Add more pythonic solution

--- is_subsequence.py
+++ is_subsequence.py
@@ -2,14 +2,8 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) > len(t):
             return False
-        i = j = 0
-        while i < len(t) and j < len(t):
-            if t[i] == s[j]:
-                j += 1
-            i += 1
-
-        return j == len(s)
-
+        it = iter(t)
+        return all(elem in it for elem in s)
 
 def main():
     s = "abc"
