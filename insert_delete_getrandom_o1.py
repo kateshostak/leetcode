24419f913e9189b83b7d524670924d7056fa7449
trina <31892809+kateshostak@users.noreply.github.com>
2020-06-15 23:53:03 +0300

    380

--- insert_delete_getrandom_o1.py
+++ insert_delete_getrandom_o1.py
@@ -12,17 +12,36 @@ class RandomizedSet:
             return False
         self.arr.append(val)
         self.d[val] = len(self.arr) - 1
-        self.len_ = len(self.arr)
         return True
 
     def remove(self, val: int) -> bool:
+        if val == 2:
+            pdb.set_trace()
         if val in self.d:
             i = self.d[val]
             self.arr[i] = self.arr[-1]
+            v = self.arr[-1]
+            self.d[v] = i
             self.d.pop(val)
-            self.len_ -= 1
+            self.arr.pop(-1)
             return True
         return False
 
     def getRandom(self) -> int:
-        return(self.arr[random.randint(0, self.len_)])
+        print(self.arr)
+        return(self.arr[random.randint(0, len(self.arr) - 1)])
+
+
+def main():
+    comms = ["insert", "insert", "remove", "insert", "remove", "getRandom"]
+    args = [[0], [1], [0], [2], [1], []]
+    sol = RandomizedSet()
+    res = []
+    for comm, arg in zip(comms, args):
+        res.append(getattr(sol, comm)(*arg))
+
+    print(f'expected::[null, true, true, true, true, true, 2], got::{res}')
+
+
+if __name__ == '__main__':
+    main()
