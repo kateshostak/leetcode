e7b709a3d8deba96c831b0b99d5aac39b5f9a919
trina <31892809+kateshostak@users.noreply.github.com>
2020-06-16 01:05:20 +0300

    O(n) solution with musch less memmory usage

--- random_pick_with_weight.py
+++ random_pick_with_weight.py
@@ -5,21 +5,17 @@ from typing import List
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.start_to_index = {}
-        self.start_to_end = {}
-        start = 0
-        self.len_ = 0
-        for i, elem in enumerate(w):
-            self.start_to_index[start] = i
-            self.start_to_end[start] = start + elem
-            start += elem
-            self.len_ = start
+        self.w = w
+        self.sum = sum(w)
 
     def pickIndex(self) -> int:
-        j = random.randint(0, self.len_ - 1)
-        for start, end in self.start_to_end.items():
-            if j >= start and j < end:
-                return self.start_to_index[start]
+        j = random.randint(1, self.sum)
+        i = 0
+        while True:
+            j -= self.w[i]
+            if j <= 0:
+                return i
+            i += 1
 
 
 def main():
