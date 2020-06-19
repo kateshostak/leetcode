66e55371b8c810078fd9e396cea4158cee01142b
trina <31892809+kateshostak@users.noreply.github.com>
2020-06-19 21:38:54 +0300

    wip

--- h_index_2.py
+++ h_index_2.py
@@ -5,15 +5,19 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         start = 0
         end = len(citations)
-        n = end - 1
+        n = end
         max_ = 0
         while start < end:
             mid = (start + end)//2
-            if citations[mid] > n - mid:
+            print(max_)
+            if citations[mid] < n - mid:
+                max_ = max(max_, citations[mid])
+                start = mid + 1
+            elif citations[mid] > n - mid:
                 end = mid
             else:
-                max_ = max(citations[mid], max_)
-                start = mid + 1
+                max_ = max(max_, citations[mid])
+                break
         return max_
 
 def main():
@@ -21,6 +25,9 @@ def main():
     res = Solution().hIndex(citations)
     print(f'expected::3, got::{res}')
 
+    citations = [11, 15]
+    res = Solution().hIndex(citations)
+    print(f'expected::2, got::{res}')
 
 if __name__ == '__main__':
     main()
