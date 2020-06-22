d17be37ad1e5877cf1cdb387217c7a0a3c7ac7c7
trina <31892809+kateshostak@users.noreply.github.com>
2020-06-23 02:10:23 +0300

    small fix

--- 16_valid_parenthesis_string.py
+++ 16_valid_parenthesis_string.py
@@ -3,15 +3,26 @@ import pdb
 
 class Solution():
     def checkValidString(self, s):
-        low = high = 0
-        for c in s:
-            low += 1 if c == '(' else -1
-            high += 1 if c != ')' else -1
-            if high < 0:
-                break
-            low = max(low, 0)
+        ast = 0
+        stack = []
+        for elem in s:
+            if elem == '(':
+                stack.append(elem)
+            else:
+                if elem == ')' and stack:
+                    stack.pop()
+                elif elem == ')' and ast != 0:
+                    ast -= 1
+                elif elem == ')':
+                    return False
 
-        return low == 0
+                else:
+                    ast += 1
+                    if stack:
+                        stack.pop()
+                        ast += 1
+        return not stack
+
 
 def main():
     # Input: "()"
@@ -22,8 +33,21 @@ def main():
 
     # Input: "(*))"
     # Output: True
+    s = '()'
+    res = Solution().checkValidString(s)
+    print(f'expected::True, got::{res}')
+
+    s = '(*)'
+    res = Solution().checkValidString(s)
+    print(f'expected::True, got::{res}')
+
+    s = '(*))'
+    res = Solution().checkValidString(s)
+    print(f'expected::True, got::{res}')
+
     s = '(*()'
-    print(Solution().checkValidString(s))
+    res = Solution().checkValidString(s)
+    print(f'expected::True, got::{res}')
 
 
 if __name__ == '__main__':
