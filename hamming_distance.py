cf946c9b2067b22697dd02b7c2472c9eadc79675
trina <31892809+kateshostak@users.noreply.github.com>
2020-07-13 03:01:39 +0300

    Solution using bitwise operations

--- hamming_distance.py
+++ hamming_distance.py
@@ -1,3 +1,20 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-       return sum(map(int,format(x^y, 'b')))
+        z = x ^ y
+        i = 0
+        while z != 0:
+            if z & 1:
+                i += 1
+            z >>= 1
+        return i
+
+
+def main():
+    x = 1
+    y = 4
+    res = Solution().hammingDistance(x, y)
+    print(f'expected::2, got::{res}')
+
+
+if __name__ == '__main__':
+    main()
