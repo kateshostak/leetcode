5e8504c75dea8de80bf7d99d9c2fc0d481a9873c
trina <31892809+kateshostak@users.noreply.github.com>
2020-07-22 01:56:32 +0300

    79

--- word_search.py
+++ word_search.py
@@ -4,49 +4,81 @@ from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def traverse(i, j, k):
-            if board[i][j] == '#':
-                return True
-            if board[i][j] != word[k]:
+            if board[i][j] == '#' or board[i][j] != word[k]:
                 return False
-
+            if k == len(word) - 1:
+                return True
             tmp = board[i][j]
             board[i][j] = '#'
             if k + 1 < len(word):
                 if i + 1 < len(board):
-                    if not traverse(i + 1, j, k + 1):
-                        return False
+                    if traverse(i + 1, j, k + 1):
+                        board[i][j] = tmp
+                        return True
 
-                if i - 1 > 0:
-                    if not traverse(i - 1, j, k + 1):
-                        return False
+                if i - 1 >= 0:
+                    if traverse(i - 1, j, k + 1):
+                        board[i][j] = tmp
+                        return True
 
                 if j + 1 < len(board[0]):
-                    if not traverse(i, j + 1, k + 1):
-                        return False
+                    if traverse(i, j + 1, k + 1):
+                        board[i][j] = tmp
+                        return True
 
-                if j - 1 > 0:
-                    if not traverse(i, j - 1, k + 1):
-                        return False
+                if j - 1 >= 0:
+                    if traverse(i, j - 1, k + 1):
+                        board[i][j] = tmp
+                        return True
             board[i][j] = tmp
-            return True
+            return False
 
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    if not traverse(i, j, 0):
-                        return False
-        return True
+                    if traverse(i, j, 0):
+                        return True
+        return False
 
 
 def main():
-    board =
-    [
-        ['A','B','C','E'],
-        ['S','F','C','S'],
-        ['A','D','E','E']
+    board = [
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E']
     ]
     word = 'ABCCED'
     res = Solution().exist(board, word)
     print(f'expected::True, got::{res}')
 
+    board = [
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E']
+    ]
+    word = 'SEE'
+    res = Solution().exist(board, word)
+    print(f'expected::True, got::{res}')
 
+    board = [
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E']
+    ]
+    word = 'ABCB'
+    res = Solution().exist(board, word)
+    print(f'expected::False, got::{res}')
+
+    board = [['a', 'b']]
+    word = 'ba'
+    res = Solution().exist(board, word)
+    print(f'expected::True, got::{res}')
+
+    board = [['C', 'A', 'A'], ['A', 'A', 'A'], ['B', 'C', 'D']]
+    word = 'AAB'
+    res = Solution().exist(board, word)
+    print(f'expected::True, got::{res}')
+
+
+if __name__ == '__main__':
+    main()
