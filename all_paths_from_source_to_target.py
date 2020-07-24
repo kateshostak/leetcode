1157f467563a03af1d6b56f552e6eb56c95d6268
trina <31892809+kateshostak@users.noreply.github.com>
2020-07-24 23:55:36 +0300

    797

--- all_paths_from_source_to_target.py
+++ all_paths_from_source_to_target.py
@@ -18,16 +18,13 @@ class Solution:
         traverse(0)
         return paths
 
+
 def main():
     graph = [[1, 2], [3], [3], []]
     res = Solution().allPathsSourceTarget(graph)
     print(f'expected::[[0, 1, 3], [0, 2, 3], got::{res}')
 
     graph = [[4, 3, 1], [3, 2, 4], [3], [4], []]
-    res = Solution().allPathsSourceTarget(graph)
-    print(f'expected::[[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]], got::{res}')
-
-    graph = [[4, 3, 11, 5, 7, 8, 10, 2, 1, 6], [2, 9, 3, 11, 10, 6, 7, 4, 5], [10, 7, 9, 4, 3, 6, 11, 5], [9, 4, 11, 6, 8, 10, 7, 5], [5, 7, 6, 9, 8, 11, 10], [8, 7, 11, 9, 10, 6], [10, 7, 11, 9, 8], [9, 10, 11, 8], [10, 9], [11, 10], [11], []] # noqa
     res = Solution().allPathsSourceTarget(graph)
     print(f'expected::[[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]], got::{res}')
 
