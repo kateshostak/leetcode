f8ef233edf8b294fe815175c0c4f8798d6f578bb
trina <31892809+kateshostak@users.noreply.github.com>
2020-07-24 23:52:45 +0300

    wip

--- all_paths_from_source_to_target.py
+++ all_paths_from_source_to_target.py
@@ -3,20 +3,20 @@ from typing import List
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        self.tmp = ''
+        tmp = []
         paths = []
 
         def traverse(node):
+            tmp.append(node)
             if node == len(graph) - 1:
-                return True
-            for n in graph[node]:
-                self.tmp += str(n)
-                if traverse(n):
-                    paths.append(self.tmp)
-                self.tmp = self.tmp[:-1]
-        traverse(0)
-        return [[0] + list(map(int, elem)) for elem in paths]
+                paths.append(tmp.copy())
 
+            for elem in graph[node]:
+                traverse(elem)
+                if tmp:
+                    tmp.pop(-1)
+        traverse(0)
+        return paths
 
 def main():
     graph = [[1, 2], [3], [3], []]
@@ -24,6 +24,10 @@ def main():
     print(f'expected::[[0, 1, 3], [0, 2, 3], got::{res}')
 
     graph = [[4, 3, 1], [3, 2, 4], [3], [4], []]
+    res = Solution().allPathsSourceTarget(graph)
+    print(f'expected::[[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]], got::{res}')
+
+    graph = [[4, 3, 11, 5, 7, 8, 10, 2, 1, 6], [2, 9, 3, 11, 10, 6, 7, 4, 5], [10, 7, 9, 4, 3, 6, 11, 5], [9, 4, 11, 6, 8, 10, 7, 5], [5, 7, 6, 9, 8, 11, 10], [8, 7, 11, 9, 10, 6], [10, 7, 11, 9, 8], [9, 10, 11, 8], [10, 9], [11, 10], [11], []] # noqa
     res = Solution().allPathsSourceTarget(graph)
     print(f'expected::[[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]], got::{res}')
 
