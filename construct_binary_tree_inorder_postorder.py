dd7f4cbe6c40cdb9bcba0be14a59df47e02f98ae
trina <31892809+kateshostak@users.noreply.github.com>
2020-07-28 03:20:14 +0300

    wip

--- construct_binary_tree_inorder_postorder.py
+++ construct_binary_tree_inorder_postorder.py
@@ -1,7 +1,6 @@
 from typing import List
 import pdb
 
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -14,22 +13,25 @@ class Solution:
         postorder.reverse()
         root_val = postorder[0]
         root_val_ind = inorder.index(root_val)
-        left_tree_val = inorder[root_val_ind - 1]
-        j = postorder.index(left_tree_val)
+        left_val = -1
+        if root_val_ind != 0:
+            left_val = inorder[root_val_ind - 1]
+            left_ind = postorder.index(left_val)
+
         def traverse(i, j):
             if i >= j:
                 return None
             node = TreeNode(postorder[i])
             node.left = traverse(i*2 + 1, j)
-            node.right = traverse(i*2 + 2, j)
+            node.right = traverse(i*2, j)
             return node
         root = TreeNode(root_val)
-        root.right = traverse(0, j)
-        root.left = traverse(j, len(postorder))
-        print(root.val)
-        print(root.left.val)
-        print(root.right.val)
-        print(self.inorder(root))
+        if left_val != -1:
+            root.right = traverse(1, left_ind)
+            root.left = traverse(left_ind, len(postorder))
+        else:
+            root.right = traverse(1, len(postorder))
+        return root
 
     def inorder(self, root):
         res = []
@@ -47,7 +49,8 @@ class Solution:
 def main():
     inorder = [9, 3, 15, 20, 7]
     postorder = [9, 15, 7, 20, 3]
-    Solution().buildTree(inorder, postorder)
+    root = Solution().buildTree(inorder, postorder)
+    print(Solution().inorder(root))
 
 
 if __name__ == '__main__':
