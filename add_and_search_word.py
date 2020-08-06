7f0dce8a1113fb1ad128c016849f8ac740e31b32
trina <31892809+kateshostak@users.noreply.github.com>
2020-08-06 19:33:04 +0300

    211

--- add_and_search_word.py
+++ add_and_search_word.py
@@ -26,11 +26,15 @@ class WordDictionary:
 
         def traverse(node, i):
             if i >= len(word):
-                return True
+                if node.end_of_word:
+                    return True
+                return False
+
             if word[i] == '.':
                 for child in [elem for letter, elem in node.children.items() if elem is not None]:
                     if traverse(child, i + 1):
                         return True
+                return False
             if not node.children[word[i]]:
                 return False
             return traverse(node.children[word[i]], i + 1)
@@ -57,6 +61,20 @@ def main():
 
     res1 = obj.search("d..")
     print(f'expected::True, got::{res1}')
+
+    res1 = obj.search("....")
+    print(f'expected::False, got::{res1}')
+
+    commands = ["addWord", "addWord", "addWord", "addWord", "search", "search", "addWord", "search", "search", "search", "search", "search", "search"]
+    args = [["at"], ["and"], ["an"], ["add"], ["a"], [".at"], ["bat"], [".at"], ["an."], ["a.d."], ["b."], ["a.d"], ["."]]
+    res = [None, None, None, None, False, False, None, True, True, False, False, True, False]
+    obj = WordDictionary()
+    i = 0
+    for comm, arg in zip(commands, args):
+        tmp = getattr(obj, comm)(*arg)
+        print(f'expected::{res[i]}, got::{tmp}, word::{arg} ')
+        i += 1
+
 
 if __name__ == '__main__':
     main()
