81c8aacdd72dcf18b717e79de9aec511df3bbfcb
trina <31892809+kateshostak@users.noreply.github.com>
2020-09-02 01:27:44 +0300

    wip

--- largest_time_for_given_digits.py
+++ largest_time_for_given_digits.py
@@ -1,68 +1,21 @@
+import itertools
 from typing import List
 
 
 class Solution:
     def largestTimeFromDigits(self, A: List[int]) -> str:
-        res = []
-        first_h = [2, 1, 0]
-        second_h = [3, 2, 1, 0]
-        first_m = [5, 4, 3, 2, 1, 0]
-        second_m = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-        for digit in first_h:
-            if digit in A:
-                res.append(digit)
-                A.remove(digit)
-                break
-        if not res:
-            return ""
-        print(res)
-        if res[0] == 2:
-            for digit in second_h:
-                if digit in A:
-                    res.append(digit)
-                    A.remove(digit)
-                    break
+        max_time = 0
+        for i, j, k, q in itertools.permutations(A):
+            hours = i * 10 + j
+            minutes = k * 10 + q
+            if hours < 24 and minutes < 60:
+                max_time = max(hours * 100 + minutes, max_time)
 
-            res.append(':')
-
-            for digit in first_m:
-                if digit in A:
-                    res.append(digit)
-                    A.remove(digit)
-                    break
-            for digit in second_m:
-                if digit in A:
-                    res.append(digit)
-                    A.remove(digit)
-                    break
-        else:
-            print(res)
-            for digit in second_m:
-                if digit in A:
-                    res.append(digit)
-                    A.remove(digit)
-                    break
-
-            res.append(':')
-
-            for digit in first_m:
-                if digit in A:
-                    res.append(digit)
-                    A.remove(digit)
-                    break
-            for digit in second_m:
-                if digit in A:
-                    res.append(digit)
-                    A.remove(digit)
-                    break
-        if len(res) < 5:
-            return ""
-
-        return ''.join(map(str, res))
+        return str(max_time % 100) * ':' + str(max_time // 100)
 
 
 def main():
-    A = [2, 0, 6, 6]
+    A = [0, 6, 2, 6]
     res = Solution().largestTimeFromDigits(A)
     print(f'expected::"06:26", got::{res}')
 
