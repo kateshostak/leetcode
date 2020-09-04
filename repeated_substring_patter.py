43a832e720d38cb97a4d925e8dca2eb8c7b5007a
trina <31892809+kateshostak@users.noreply.github.com>
2020-09-04 03:08:30 +0300

    459 Replace previous solution with a more clever one

--- repeated_substring_patter.py
+++ repeated_substring_patter.py
@@ -1,34 +1,8 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        if len(s) <= 1:
-            return False
-        if s[-1]*len(s) == s:
-            return True
-        start = 1
-        end = len(s) - 1
-        while start < len(s):
-            i = s[start:].find(s[-1]) + start
-            j = end
-            if i == j:
-                return False
-            start = i + 1
-            if self.check_subs(s, start):
-                return True
-        return False
-
-    def check_subs(self, s, start):
-        subs = s[:start]
-        i = start
-        j = 0
-        while i < len(s):
-            j %= len(subs)
-            if subs[j] != s[i]:
-                return False
-            i += 1
-            j += 1
-
-        return j == len(subs)
-
+        tmp = s
+        s += s
+        return s[1:-1].find(tmp) != -1
 
 def main():
     s = 'abcabcabcabc'
