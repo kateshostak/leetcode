7c687e14dc5495268880716e59718d3169e90200
trina <31892809+kateshostak@users.noreply.github.com>
2020-10-18 00:01:20 +0300

    189

--- rotate_array.py
+++ rotate_array.py
@@ -3,21 +3,40 @@ from typing import List
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         k %= len(nums)
-        if k == 0:
-            return
-        else:
-            tmp = nums[len(nums) - k:] + nums[:len(nums) - k]
-            for i in range(len(nums)):
-                nums[i] = tmp[i]
+        i = 0
+        head = 0
+        print(nums)
+        for _ in range(len(nums) - 1):
+            shift = (len(nums) - k + i) % len(nums)
+            if shift == head:
+                i = shift + 1
+                head = i
+                continue
+            nums[i], nums[shift] = nums[shift], nums[i]
+            i = shift
+
+        return nums
+
 
 def main():
-    # Input: [1, 2, 3, 4, 5, 6, 7] and k = 3
-    # Output: [5, 6, 7, 1, 2, 3, 4]
     nums = [1, 2, 3, 4, 5, 6, 7]
     k = 3
-    Solution().rotate(nums, k)
-    print(nums)
+    res = Solution().rotate(nums, k)
+    print(res)
+
+    nums = [-1, -100, 3, 99]
+    k = 2
+    res = Solution().rotate(nums, k)
+    print(res)
+
+    nums = [1, 2, 3, 4, 5, 6]
+    k = 2
+    res = Solution().rotate(nums, k)
+    print(res)
 
 
 if __name__ == '__main__':
