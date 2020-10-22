3c0a458087ea8fbfb1c54cc9d14d3218ef797063
trina <31892809+kateshostak@users.noreply.github.com>
2020-10-22 23:22:24 +0300

    wip

--- minimum_domino_rotations_for_equal_row.py
+++ minimum_domino_rotations_for_equal_row.py
@@ -5,30 +5,33 @@ class Solution:
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
         if len(A) <= 1:
             return -1
-        r = 0
-        prev1 = A[0]
-        prev2 = B[0]
-        for i in range(1, len(A)):
-            if A[i] == prev1 and B[i] == prev2:
-                continue
-            if A[i] == prev1 and B[i] != prev2:
-                prev2 = None
-                continue
-            if B[i] == prev2 and A[i] != prev1:
-                prev1 = None
-                continue
-            if A[i] == prev2:
-                r += 1
-                if B[i] != prev1:
-                    prev1 = None
-                continue
-            if B[i] == prev1:
-                r += 1
-                if A[i] != prev2:
-                    prev2 = None
-                continue
-            return -1
-        return r
+
+        def find_min(prev1, prev2):
+            r1 = 0
+            r2 = 0
+            for i in range(1, len(A)):
+                if A[i] != prev1:
+                    if B[i] != prev1:
+                        r1 = None
+                        break
+                    r1 += 1
+
+            for i in range(1, len(B)):
+                if B[i] != prev2:
+                    if A[i] != prev2:
+                        r2 = None
+                        break
+                    r2 += 1
+
+            print(r1, r2)
+            if r1 is None and r2 is None:
+                return -1
+            if r1 is None:
+                return r2
+            if r2 is None:
+                return r1
+            return min(r1, r2)
+        return min(find_min(A[0], B[0]), find_min(B[0], A[0]))
 
 
 def main():
@@ -36,6 +39,16 @@ def main():
     B = [3, 2, 3, 1, 3, 2, 3, 3, 2]
     res = Solution().minDominoRotations(A, B)
     print(f'expected:: -1, got:: {res}')
+
+    A = [2, 1, 2, 4, 2, 2]
+    B = [5, 2, 6, 2, 3, 2]
+    res = Solution().minDominoRotations(A, B)
+    print(f'expected:: 2, got:: {res}')
+
+    A = [1, 1, 2, 1, 1, 1, 2, 1, 2]
+    B = [2, 2, 1, 2, 2, 2, 1, 1, 1]
+    res = Solution().minDominoRotations(A, B)
+    print(f'expected:: 3, got:: {res}')
 
 
 if __name__ == '__main__':
