b9ff3c00d72bb32910a55984b2a4503ba411c7a4
trina <31892809+kateshostak@users.noreply.github.com>
2020-11-06 03:29:23 +0300

    1217 One-liner

--- minimum_cost_to_move_chips_to_the_same_pos.py
+++ minimum_cost_to_move_chips_to_the_same_pos.py
@@ -3,12 +3,4 @@ from typing import List
 
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
-        odd_count = 0
-        even_count = 0
-        for i in range(len(position)):
-            if position[i] % 2 == 0:
-                even_count += 1
-            else:
-                odd_count += 1
-
-        return min(odd_count, even_count)
+        return min((odd :=sum(p % 2 for p in position)), len(position) - odd)
